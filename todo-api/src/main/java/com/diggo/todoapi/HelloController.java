0f721338af08938669ef57424b5ca125e3da2e69
smj <smj123432@gmail.com>
2026-09-09 15:55:54 +0900

    Day 2: Spring Boot 프로젝트 생성 및 첫 API 구현

---
+++ todo-api/src/main/java/com/diggo/todoapi/HelloController.java
@@ -0,0 +1,13 @@
+package com.diggo.todoapi;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "안녕하세요, 형님!";
+    }
+}

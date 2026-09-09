d63b8d284c0512c7e057ea6729c9c8b843c9dd06
smj <smj123432@gmail.com>
2026-09-09 13:59:41 +0900

    자바 기초 문법 공부 = 스트림

--- src/StreamExample.java
+++ src/StreamExample.java
@@ -1,12 +1,16 @@
-public class StreamExample {
-    interface Greeting{
-        void sayHello(String name);
-    }
+import java.util.List;
 
+public class StreamExample {
     public static void main(String[] args){
-        Greeting greeting = (name) -> {
-            System.out.println(name + "님 안녕하세요");
-        };
-        greeting.sayHello("형님");
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5);
+        List<Integer> evenNumbers = numbers.stream()
+                .filter(n -> n % 2 == 0)
+                .toList();
+        System.out.println(evenNumbers);
+
+        List<Integer> doubled = numbers.stream()
+                .map(n -> n * 2)
+                .toList();
+        System.out.println(doubled);
     }
 }
